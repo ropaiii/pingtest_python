43abc04bb3d27f29469c19076f9d84f38e7f3fb4
mail@mail.com <mail@mail.com>
2026-01-14 17:54:35 +0100

    Created pingtest script
    Created pingtest script

---
+++ pingtest.py
@@ -0,0 +1,56 @@
+import subprocess
+import sys
+
+def pingtest_run(ip : str) -> bool:
+    cmd : str = f"ping {ip}"
+    print(f"Pinging {ip}...")
+    try:
+        output : int = subprocess.call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=5)
+        if output == 0:
+            return True
+        else:
+            return False
+    except subprocess.TimeoutExpired:
+        return False
+    except Exception:
+        return False
+
+
+def main(filename : str) -> None:
+    successip : list = list()
+    failsip : list = list()
+
+    try:
+        with open(filename, "r") as file:
+            ipadresslist : list = [line.strip() for line in file if line.strip()]
+
+        if not ipadresslist:
+            print("No IP-adresses found!!!!")
+            return
+
+        for ip in ipadresslist:
+            if pingtest_run(ip):
+                print("Ping test successful!!!")
+                successip.append(ip)
+
+            else:
+                print("Ping test failed!!!")
+                failsip.append(ip)
+
+        print("\nSuccessful IP-adresses:")
+        for i in successip:
+            print(i)
+        print("\nFailed IP-adresses:")
+        for i in failsip:
+            print(i)
+    except Exception as e:
+        print(e)
+
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+        print("Usage: python pingtest.py filename")
+        sys.exit(1)
+    filename = sys.argv[1]
+    main(filename)
